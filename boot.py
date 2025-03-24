ba367e11f2361af1f3986a0343486e99d27a4a27
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2025-03-24 21:33:16 +0530

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,21 @@
+import asyncio
+from pyrogram import Client, idle
+from database import db
+from configs import Telegram 
+
+async def start_client(token):
+    client = Client(":memory:", Telegram.API_ID, Telegram.API_HASH, bot_token=token, plugins={"root": "handlers"})
+    try:
+        await client.start()
+        await idle()
+    except:
+        pass 
+
+async def main():
+    bot_tokens = await db.fetch_all("tokens")  
+    tasks = [start_client(token) for token in bot_tokens]
+    await asyncio.gather(*tasks)
+    print('All clients booted')
+
+if __name__ == "__main__":
+    asyncio.run(main())
