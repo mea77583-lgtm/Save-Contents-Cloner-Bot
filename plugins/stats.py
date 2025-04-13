09cf37f675b45ed513652c7d98783dccacdad2a1
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2025-04-13 15:05:19 +0530

    Update stats.py

--- plugins/stats.py
+++ plugins/stats.py
@@ -7,5 +7,6 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 
 @Client.on_message(filters.command("users") & filters.private)
 async def users_handler(client: Client, message: Message):
-    user_count = len(await db.fetch_all("users"))
-    await message.reply(f"👥 **Total Users:** {user_count}")
+    bot_info = await Client.get_me()
+    user_count = len(await db.fetch_all(f"{bot_info.username}users"))
+    await message.reply(f"👥 **Total Users on this bot:** {user_count}")
